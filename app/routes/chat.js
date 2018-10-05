0ffcbea15d7a8a98c79b941fa17d87adbf8bdd87
Douglas <senhorsatou@gmail.com>
2018-10-04 21:24:49 -0300

    update

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,45 +1,50 @@
 module.exports= function(app){
 
-	const VoiceResponse = require('twilio').twiml.VoiceResponse;
-
-const response = new VoiceResponse();
-response.say(
-  {
-    voice: 'woman',
-    language: 'br',
-  },
-  'Bem vindo a allmatech'
-);
-
-app.get('/chat', function(req ,res){
-	var dadosForm = req.body;
 
 
-
-
-		app.get('io').emit(
-			'msgParaCliente',
-			{apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'}
-		)
-
+	app.get('/chat', function(req ,res){
+		var dadosForm = req.body;
+	
+	
+	
+	
+			app.get('io').emit(
+				'msgParaCliente',
+				{apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'}
+			)
+	
+			res.render("chat", {dadosForm : dadosForm});
+	
+	});
+	app.post('/chat', function(req ,res){
+		var dadosForm = {
+			apelido : req.body.apelido,
+			email : req.body.email,
+			telefone : req.body.telefone,
+			tipo : req.body.tipo,
+			cidade : req.body.cidade
+		};
+	
 		res.render("chat", {dadosForm : dadosForm});
+	
+	});
+	
+	app.post('/xml',(req, res) =>{ 
+		const VoiceResponse = require('twilio').twiml.VoiceResponse;
+	
+	const response = new VoiceResponse();
+	response.say(
+		{
+			voice: 'woman',
+			language: 'br',
+		},
+		'Bem vindo a allmatech'
+	);
 
-});
-app.post('/chat', function(req ,res){
-	var dadosForm = {
-	  apelido : req.body.apelido,
-	  email : req.body.email,
-	  telefone : req.body.telefone,
-	  tipo : req.body.tipo,
-	  cidade : req.body.cidade
-	};
+	res.writeHead(200, {'Content-Type' : 'text/xml'})
+	res.end(response.toString());
+	})
+	
 
-	res.render("chat", {dadosForm : dadosForm});
-
-});
-
-app.get('/xml',(req, res) =>{ 
-	res.set('Content-Type', 'text/xml');
-    res.send(response.toString());
-})
-}
+	}
+	
